3727f08b6b082dc388ecdb64d82b846b50a6ebc2
Anumeenakshi Srinivasa Subramanian <Anumeenakshi.SrinivasaSubramanian@fisglobal.com>
2021-04-23 15:32:46 +0530

    Case study

--- src/app/dashboard/dashboard-detail/edit-detail/edit-detail.component.ts
+++ src/app/dashboard/dashboard-detail/edit-detail/edit-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IUsers } from 'src/app/users';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from 'src/app/api.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-detail',
@@ -14,6 +15,7 @@ export class EditDetailComponent implements OnInit {
   userName: any;
   users: IUsers[];
   user: IUsers;
+  editForm:  any;
 
   constructor(private route: ActivatedRoute, private api : ApiService, private router : Router) { }
 
@@ -23,21 +25,40 @@ export class EditDetailComponent implements OnInit {
     this.api.getUserByUserName(this.userName).subscribe(
       (response) => {
         this.users = response;
+        this.user = this.users.find(t=>t.userName==this.userName)
+        console.log("edit user: "+this.user.firstName);
+        this.editForm.patchValue(this.user);
       }
     );
+    this.editForm = new FormGroup({
+      id: new FormControl(""),
+      firstName: new FormControl("", [Validators.pattern('^[a-zA-Z]+$')]),
+      lastName: new FormControl("", [Validators.pattern('^[a-zA-Z]+$')]),
+      dob: new FormControl(""),
+      email: new FormControl("", [Validators.email]),
+      addressLine1: new FormControl(""),
+      addressLine2: new FormControl(""),
+      city: new FormControl("", [Validators.pattern('^[a-zA-Z]+$')]),
+      state: new FormControl("", [Validators.pattern('^[a-zA-Z]+$')]),
+      country: new FormControl("", [Validators.pattern('^[a-zA-Z]+$')]),
+      zipCode: new FormControl(""),
+      userName: new FormControl(""),
+      password: new FormControl(""),
+    });
+
     
   }
   backToDashboard () {
     this.router.navigate(['/dashboard']);
   }
-  saveChanges(data) {
-    alert("User Details updated")
-    console.log("update:" +data.userName);
+  onSubmit(data) {
+    alert("User Details updated!!")
+    console.log("update:" +data.id);
     this.api.getUserByUserName(data.userName).subscribe(
       (response) => {
         this.users = response;
+        console.log(response)
         this.user =  this.users.find(t=>t.userName === data.userName);
-        this.id = this.user.id;
         console.log("User - firstName: "+this.user.firstName)
         console.log("Data - firstName: "+data.firstName)
         this.api.updateUser(this.user,this.id,data).subscribe (
@@ -49,6 +70,8 @@ export class EditDetailComponent implements OnInit {
         console.log(error);
       }
     );
-    this.router.navigate(['/dashboard']);
+    
+    
   }
+
 }
